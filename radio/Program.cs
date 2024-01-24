76b8def2f36a59d62ede64aac51a07bc2f5cea2f
Utente <Utente@DESKTOP-BAB4AAS>
2024-01-24 16:17:08 +0100

    Aggiungere i file di progetto.

---
+++ radio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio r=new Radio();
+            r.Volume=float.Parse(Console.ReadLine());
+
+            Console.WriteLine("aumenta il volume:{0}", r.Aumentavol());
+            Console.WriteLine("diminuisci il volume: {0}", r.Diminuiscivol());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ radio/Radio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radio
+{
+    internal class Radio
+    {
+        public float Volume { get; set; }
+
+        public float Aumentavol()
+        {
+            Volume=(float)(Volume+Volume*0.1);
+            return Volume;
+        }
+
+        public float Diminuiscivol()
+        {
+            Volume = (float)(Volume - (Volume * 0.1));
+            return Volume;
+        }
+    }
+}
